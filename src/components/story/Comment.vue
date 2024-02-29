4cded8823fd178d46d7249e6178820fa1a181136
William <webuntermench@protonmail.com>
2024-02-29 18:27:52 +0300

    Add expanded visible

--- src/components/story/Comment.vue
+++ src/components/story/Comment.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import CaretLeft from '@/components/ui/icons/CaretLeft.vue'
+import CaretDown from '@/components/ui/icons/CaretDown.vue'
 
 withDefaults(defineProps<{
   node: any
@@ -9,6 +11,7 @@ withDefaults(defineProps<{
 })
 
 const isExpanded = ref(false)
+
 function clickHandler() {
   isExpanded.value = !isExpanded.value
 }
@@ -16,13 +19,21 @@ function clickHandler() {
 
 <template>
   <div class="text-sm my-3">
-    <div :style="{ 'margin-left': `${depth * 20}px` }" @click="clickHandler" v-html="node.title" />
-    <div class="flex text-muted-foreground gap-4 text-xs">
-      <h6 class="pl-2" :style="{ 'margin-left': `${depth * 20}px` }">
+    <div :style="{ 'margin-left': `${depth * 20}px` }" v-html="node.title" />
+    <ul class="flex text-muted-foreground gap-4 text-xs">
+      <li class="pl-2" :style="{ 'margin-left': `${depth * 20}px` }">
         — {{ node.by }}
-      </h6>
-      <span>{{ new Date(node.time).toLocaleTimeString() }}</span>
-    </div>
+      </li>
+      <li>{{ new Date(node.time).toLocaleTimeString() }}</li>
+      <li class="hover:underline cursor-pointer flex items-center" @click="clickHandler">
+        <template v-if="isExpanded">
+          close comments <span><CaretDown /></span>
+        </template>
+        <template v-else>
+          load more comments <span><CaretLeft /></span>
+        </template>
+      </li>
+    </ul>
 
     <div v-if="isExpanded">
       <Comment v-for="child in node.children" :key="child.title" :node="child" :depth="depth + 1" />
